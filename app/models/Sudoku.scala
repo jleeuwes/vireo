30f88ba44a4fbbf00895a749398aa60875c407e8
Jeroen Leeuwestein <jeroenleeuwestein@gmail.com>
2014-06-26 00:12:44 +0200

    Some experimentation with storing possibilities.

--- app/models/Sudoku.scala
+++ app/models/Sudoku.scala
@@ -1,8 +1,34 @@
+// def catOptions [A, T[_]] (xs: Traversable[Option[A]])(implicit cast:
+// Traversable[A] => T[A]) : T[A] = {
+def catOptions [A] (xs: Traversable[Option[A]]) : Traversable[A] = {
+  xs.filter(_.nonEmpty).map(_.get)
+}
+
 class Sudoku (symbols: Set[Int], values: Vector[Option[Int]]) {
   val nrOfSymbols         = symbols.size
   private val blockSideD  = scala.math.sqrt(nrOfSymbols)
   val blockSide           = blockSideD.toInt
   val requiredTableSize   = nrOfSymbols * nrOfSymbols
+  
+  // These store possibilities left per column/row/block.
+  // This seemed like a good idea and should be faster (when mass-querying
+  // possibilities), but isn't.
+  val unit = (0 to nrOfSymbols - 1)
+  lazy val allColumns = unit.map { col =>
+    symbols -- catOptions(unit.map { row => table(index(row, col)) })
+  }
+  lazy val allRows = unit.map { row =>
+    symbols -- catOptions(unit.map { col => table(index(row, col)) })
+  }
+  val smallUnit = (0 to blockSide - 1)
+  val blocks = for (a <- smallUnit; b <- smallUnit) yield (a,b)
+  lazy val allBlocks = blocks.map { case (bRow, bCol) =>
+    val row0 = bRow * blockSide
+    val col0 = bCol * blockSide
+    symbols -- catOptions(blocks.map { case (row, col) =>
+      table(index(row0 + row, col0 + col))
+    })
+  }
 
   if (blockSide != blockSideD) {
     throw new IllegalArgumentException("nrOfSymbols should be a square")
@@ -31,6 +57,10 @@ class Sudoku (symbols: Set[Int], values: Vector[Option[Int]]) {
   def toRow(index: Int)     : Int = index / nrOfSymbols
   def toColumn(index: Int)  : Int = index % nrOfSymbols
   def toBlock(index: Int)   : Int = toRow(index) / blockSide * blockSide + toColumn(index) / blockSide
+  
+  def possibilities_(row: Int, col: Int) : Set[Int] = {
+    allRows(row) & allColumns(col) & allBlocks(toBlock(index(row, col)))
+  }
 
   def possibilities(row: Int, col: Int) : Set[Int] = {
     val here  = index(row, col)
@@ -54,4 +84,18 @@ class Sudoku (symbols: Set[Int], values: Vector[Option[Int]]) {
 
     return poss
   }
+}
+
+object Sudoku {
+  // Convenience function. Create a sudoku by specifying min and max symbol and
+  // a Vector in which cells are just an Int and empty cells are 0.
+  def make(min: Int, max: Int, values: Vector[Int]): Sudoku = {
+    return new Sudoku((min to max).toSet, values.map { n =>
+      n match {
+        case 0 => None
+        case n => Some(n)
+      }
+    })
+  }
+
 }
