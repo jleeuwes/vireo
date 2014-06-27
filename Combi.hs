24090232319843e85a764d293b792ca140f0e993
Jeroen Leeuwestein <jeroenleeuwestein@gmail.com>
2014-06-27 03:02:43 +0200

    More cleanup:
    - Document Combi.hs.
    - Make CSS static again (we didn't use any Less features anymore)
    - Add HTML content type.

--- Combi.hs
+++ Combi.hs
@@ -1,16 +1,24 @@
 module Main where
 
--- Produceer commando's om de sudoku-nummers in alle combinaties te tilen tot
--- één plaatje.
+-- Produce shell commands to tile the tiny sudoku numbers in all possible
+-- combinations. From directory public/images, run:
+--
+--   runhaskell ../../Combi.hs | sh
+--
+-- Of course, this should be done by sbt and ideally in Scala.
+-- Haskell is just so nice to use for 'scripting'.
 
 main
     = mapM_ putStrLn $ zipWith mkCommand combis bitPats
   where
+    -- All lists of length 9 in which the i'th element is either 0 or i.
     combis  = sequence [[0,x] | x <- [1..9]]
+    -- Sum the 2-powers of each non-zero element, so we get the bit patterns
+    -- used in React.
     bitPats = map (sum . map ((2^) . pred) . filter (>0)) combis
 
-    mkCommand cs bitPat = unwords
+    mkCommand combi bitPat = unwords
       ( "montage"
-      : map ((++ ".png") . show) cs
+      : map ((++ ".png") . show) combi
      ++ ["-geometry", "+1+1", "-tile", "3x3", "pat" ++ show bitPat ++ ".png"]
       )
